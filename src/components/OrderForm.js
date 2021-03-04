970e3edbf95253e6714da70cae4690a6e8799367
Alex Calia <alex.m.calia@outlook.com>
2021-03-03 21:43:58 -0500

    working on ordering page, may need to pivot

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -2,14 +2,27 @@ import React, { useState } from 'react';
 import Topping from './Topping';
 
 const OrderForm = () => {
-  const newTopping = { topping: '', price: 0 };
   const [size, setSize] = useState({});
   const [name, setName] = useState('');
   const [address, setAddress] = useState('');
   const [toppings, setToppings] = useState([]);
+  const [toppingNum, setTopNum] = useState(0);
+  const newTopping = { topping: '', price: 0 };
 
   const addTopping = () => {
-    setToppings([...toppings, { ...newTopping }]);
+    setToppings([...toppings, { ...newTopping, topNum: toppingNum }]);
+    setTopNum(toppingNum + 1);
+  };
+
+  const removeTopping = (e, topNum) => {
+    e.preventDefault();
+    const updateToppings = [...toppings];
+    let indexRem;
+    for (let i = 0; i < toppings.length; i++) {
+      if (updateToppings[i].topNum === topNum) indexRem = i;
+    }
+    updateToppings.splice(indexRem, 1);
+    setToppings(updateToppings);
   };
 
   const configTopping = (e) => {
@@ -26,6 +39,9 @@ const OrderForm = () => {
             key={`topping${index}`}
             index={index}
             configTopping={configTopping}
+            removeTopping={removeTopping}
+            topNum={topping.topNum}
+            value
           />
         ))}
       </form>
--- src/components/Topping.js
+++ src/components/Topping.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Topping = ({ index, configTopping }) => (
-  <select data-index={index} onChange={configTopping}>
-    <option value="pepperoni">Pepperoni</option>
-    <option value="olives">Olives</option>
-    <option value="peppers">Peppers</option>
-    <option value="mushrooms">Mushrooms</option>
-    <option value="onion">Onion</option>
-  </select>
+const Topping = ({ index, configTopping, removeTopping, topNum }) => (
+  <>
+    <button onClick={(e) => removeTopping(e, topNum)}>Remove</button>
+    <select data-index={index} onChange={configTopping}>
+      <option value="pepperoni">Pepperoni</option>
+      <option value="olives">Olives</option>
+      <option value="peppers">Peppers</option>
+      <option value="mushrooms">Mushrooms</option>
+      <option value="onion">Onion</option>
+    </select>
+  </>
 );
 
 export default Topping;
